4b5fb89ac3502b3063785b604b4c3d1e1438aff0
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-03-05 00:57:27 +0100

    added some features and improved code

--- script.js
+++ script.js
@@ -2,63 +2,86 @@
 let searchBar = document.getElementById("search");
 let button = document.getElementById("enter");
 let heading = document.getElementById("heading");
-let headingTwo = document.getElementById("headingTwo");
+let temp = document.getElementById("temp");
 let headingThree = document.getElementById("headingThree");
 let description = document.getElementsByClassName("description")[0];
 let humidity = document.getElementsByClassName("humidity")[0];
 let wind = document.getElementsByClassName("wind")[0];
 let icon = document.getElementById("icon");
+let buttonSwitchC = document.getElementById("toggleC");
+let buttonSwitchF = document.getElementById("toggleF");
 
 //constructor function
-function City(name, main, weather, wind) {
-  this.name = name;
-  this.main = main;
-  this.weather = weather;
-  this.wind = wind;
+
+class City {
+  constructor({ name, main, weather, wind }) {
+    this.name = name;
+    this.main = main;
+    this.weather = weather;
+    this.wind = wind;
+  }
+  static create(response) {
+    return new City(response);
+  }
+  showData() {
+    searchBar.value = "";
+    heading.textContent = `Weather in ${this.name}`;
+    temp.textContent = `${Math.floor(this.main.temp)}°C`;
+    headingThree.textContent = `Feels like: ${Math.floor(
+      this.main.feels_like
+    )}°C`;
+    buttonSwitchF.addEventListener("click", () => {
+      temp.textContent = `${(Math.floor(this.main.temp) * 9) / 5 + 32}°F`;
+      headingThree.textContent = `Feels like: ${
+        Math.floor(this.main.feels_like * 9) / 5 + 32
+      }°F`;
+    });
+    buttonSwitchC.addEventListener("click", () => {
+      temp.textContent = `${Math.floor(this.main.temp)}°C`;
+      headingThree.textContent = `Feels like: ${Math.floor(
+        this.main.feels_like
+      )}°C`;
+    });
+    icon.src =
+      "http://openweathermap.org/img/w/" + this.weather[0].icon + ".png";
+    description.textContent = this.weather[0].description;
+    humidity.textContent = `Humidity: ${this.main.humidity}%`;
+    wind.textContent = `Wind speed: ${this.wind.speed} km/h`;
+  }
 }
 
 //function to get the API data
-let storeData = (city) => {
-  return fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&appid=85005f96ed02e50e4c1697b861c0b087&units=metric"
-  )
-    .then((response) => response.json())
-    .then((response) => response);
+let storeData = async (city) => {
+  return (
+    await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=85005f96ed02e50e4c1697b861c0b087&units=metric"
+    )
+  ).json();
 };
 
-//function to show the data from the API
-let showData = (value) => {
-  searchBar.value = "";
-  //populate our newly created object
-  let city = new City(value.name, value.main, value.weather, value.wind);
-  console.log(city);
-  heading.innerHTML = `Weather in ${value.name}`;
-  headingTwo.innerHTML = `${value.main.temp}°C`;
-  headingThree.innerHTML = `Feels like: ${value.main.feels_like}°C`;
-  icon.src =
-    "http://openweathermap.org/img/w/" + value.weather[0].icon + ".png";
-  description.innerHTML = value.weather[0].description;
-  humidity.innerHTML = `Humidity: ${value.main.humidity}%`;
-  wind.innerHTML = `Wind speed: ${value.wind.speed} km/h`;
+//function to call the storeData function and later call it with the button
+let weatherAlert = async () => {
+  try {
+    let value = await storeData(searchBar.value);
+    let city = City.create(value);
+    city.showData();
+    // return value;
+  } catch (err) {
+    encounterError();
+  }
 };
 
 //refresh our HTML elements
 let encounterError = () => {
-  heading.innerHTML = "Nothing found";
-  headingTwo.innerHTML = "";
-  headingThree.innerHTML = "";
-  description.innerHTML = "";
-  humidity.innerHTML = "";
-  wind.innerHTML = "";
-};
-
-//function to call the storeData function and later call it with the button
-let weatherAlert = () => {
-  storeData(searchBar.value)
-    .then((value) => showData(value))
-    .catch((error) => encounterError());
+  heading.textContent = "Nothing found";
+  temp.textContent = "";
+  headingThree.textContent = "";
+  description.textContent = "";
+  humidity.textContent = "";
+  wind.textContent = "";
+  icon.src = "";
 };
 
 button.addEventListener("click", weatherAlert);
